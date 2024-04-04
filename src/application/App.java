6a55878896e7b77f36d430e26e14e313446572d7
vincewip <46308382+vincewip@users.noreply.github.com>
2024-04-04 12:40:09 -0500

    chore: remove unused code, fix typos

--- src/application/App.java
+++ src/application/App.java
@@ -146,7 +146,7 @@ public class App {
      */
     private static void displayMenu() {
         System.out.println(
-                "\tgcID <part of the name of customer> - Gets the Name of all the customer with 'part of the name of the customer' int their name");
+                "\tgcID <part of the name of customer> - Gets the Name of all the customer with 'part of the name of the customer' in their name");
         System.out.println(
                 "\tsc - Show all the Countries along with their Country Code");
         System.out.println(
@@ -158,7 +158,7 @@ public class App {
         System.out.println(
                 "\tspc <country limit> - Stores and Profit by Country");
         System.out.println(
-                "\ttopproducts <country code> - Top Product Holders by Category");
+                "\ttopproducts <country code> - Top Product Holders by Country");
         System.out.println(
                 "\trc <customerID>  - Customer Returned Item Count Analysis");
         System.out.println(
@@ -185,67 +185,3 @@ public class App {
 
     }
 }
-
-// class Database {
-// // private Connection connection;
-
-// public Database() {
-// // try {
-// // String url = "jdbc:sqlite:library.db";
-// // // create a connection to the database
-// // connection = DriverManager.getConnection(url);
-// // } catch (SQLException e) {
-// // e.printStackTrace(System.out);
-// // }
-
-// }
-
-// public void storeProfitByCountry(int countryLimit) {
-// System.out.println("storeProfitByCountry not implemented yet!!");
-// }
-
-// public void topProducts(String countryCode) {
-// System.out.println("topProducts not implemented for this country!");
-// }
-
-// public void returnedItemCount(String customerID) {
-// System.out.println("returnedItemCount not implemented yet!!");
-// }
-
-// public void discountedProducts(String categoryName) {
-// System.out.println("discountedProducts not implemented yet !!!");
-// }
-
-// public void shippingDetails(String orderID) {
-// System.out.println("shippingDetails not implemented yet!!");
-// }
-
-// public void salesSummaryByCategory(String categoryName) {
-// System.out.println("salesSummaryByCategory not implemented yet!!!");
-// }
-
-// public void subCategoryInventory() {
-// System.out.println("subCategoryInventory not implemented yet!!");
-// }
-
-// public void returnedProducts(String customerID) {
-// System.out.println("Returned products not implemented yet!!!");
-// }
-
-// public void returnedByRegion(String regionName) {
-// System.out.println("Returned by Region not implemented yet!!!");
-// }
-
-// public void averagePrice(String categoryID) {
-// System.out.println("averagePrice not implemented for this Category!!!");
-// }
-
-// public void exceed7() {
-// System.out.println("exceed 7 is not implemented yet!!!");
-// }
-
-// public void largestReturnedAmount() {
-// System.out.println("largestreturnedAmount is not implemented yet!!!");
-// }
-
-// }
